ec91586817e3781b5e55e744f1c2b95333ce7795
Furkan Emin <femincanx@gmail.com>
2023-01-23 13:45:14 +0300

    feat: add puzzle control controllers

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,4 +1,18 @@
 class SudokuSolver {
+  constructor() {
+    this.rowsMap = {
+      A: 0,
+      B: 1,
+      C: 2,
+      D: 3,
+      E: 4,
+      F: 5,
+      G: 6,
+      H: 7,
+      I: 8,
+    };
+  }
+
   validate(puzzleString) {
     if (!puzzleString) {
       return { result: false, message: 'Required field missing' };
@@ -20,11 +34,55 @@ class SudokuSolver {
     return { result: true };
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {}
+  checkRowPlacement(puzzleString, row, value) {
+    const startIndex = this.rowsMap[row] * 9;
+    const endIndex = startIndex + 9;
 
-  checkColPlacement(puzzleString, row, column, value) {}
+    const rowString = puzzleString.slice(startIndex, endIndex);
 
-  checkRegionPlacement(puzzleString, row, column, value) {}
+    if (rowString.includes(value.toString())) {
+      return false;
+    }
+
+    return true;
+  }
+
+  checkColPlacement(puzzleString, column, value) {
+    const columnArray = [];
+
+    for (let i = 0; i < 9; i++) {
+      columnArray.push(puzzleString[i * 9 + (column - 1)]);
+    }
+
+    if (columnArray.includes(value.toString())) {
+      return false;
+    }
+
+    return true;
+  }
+
+  checkRegionPlacement(puzzleString, row, column, value) {
+    const rowIndexOfRegion = Math.floor(this.rowsMap[row] / 3);
+    const columnIndexOfRegion = Math.floor((column - 1) / 3);
+
+    const IndexOfFirstElement = 27 * rowIndexOfRegion + 3 * columnIndexOfRegion;
+
+    const regionArray = [];
+
+    // get one row of the region on each iteration
+    for (let i = 0; i < 3; i++) {
+      // get one element of the row at each iteration
+      for (let j = 0; j < 3; j++) {
+        regionArray.push(puzzleString[IndexOfFirstElement + j + 9 * i]);
+      }
+    }
+
+    if (regionArray.includes(value.toString())) {
+      return false;
+    }
+
+    return true;
+  }
 
   solve(puzzleString) {}
 }
