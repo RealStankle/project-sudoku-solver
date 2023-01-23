0dcd3a83e0f9fdf69332a4f262922bd944b49192
Furkan Emin <femincanx@gmail.com>
2023-01-23 13:46:31 +0300

    test: add tests for puzzle control controllers

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,13 +6,13 @@ const puzzleStrings = require('../controllers/puzzle-strings');
 let solver = new Solver();
 
 suite('Unit Tests', () => {
-  test('it should return true for valid puzzle', () => {
+  test('validate function should return true for valid puzzle', () => {
     const validationResult = solver.validate(puzzleStrings[0][0]);
 
     assert.deepEqual(validationResult, { result: true });
   });
 
-  test('it should return false and error message if puzzle is not provided', () => {
+  test('validate function should return false and error message if puzzle is not provided', () => {
     const validationResult = solver.validate();
 
     assert.deepEqual(validationResult, {
@@ -21,7 +21,7 @@ suite('Unit Tests', () => {
     });
   });
 
-  test('it should return false and error message if puzzle contains invalid characters', () => {
+  test('validate function should return false and error message if puzzle contains invalid characters', () => {
     const validationResult = solver.validate(
       `${puzzleStrings[0][0].slice(0, 78)}abc`
     );
@@ -32,12 +32,74 @@ suite('Unit Tests', () => {
     });
   });
 
-  test('it should return false and error message for wrong puzzle length', () => {
+  test('validate function should return false and error message for wrong puzzle length', () => {
     const validationResult = solver.validate(puzzleStrings[0][0].slice(0, 70));
 
     assert.deepEqual(validationResult, {
       result: false,
       message: 'Expected puzzle to be 81 characters long',
     });
+  });
+
+  test('checkRowPlacement function should return true for a valid row placement', () => {
+    const validationResult = solver.checkRowPlacement(
+      puzzleStrings[0][0],
+      'D',
+      7
+    );
+
+    assert.isTrue(validationResult);
+  });
+
+  test('checkRowPlacement function should return false for an invalid row placement', () => {
+    const validationResult = solver.checkRowPlacement(
+      puzzleStrings[0][0],
+      'H',
+      3
+    );
+
+    assert.isFalse(validationResult);
+  });
+
+  test('checkColPlacement function should return true for a valid col placement', () => {
+    const validationResult = solver.checkColPlacement(
+      puzzleStrings[0][0],
+      5,
+      3
+    );
+
+    assert.isTrue(validationResult);
+  });
+
+  test('checkColPlacement function should return false for an invalid col placement', () => {
+    const validationResult = solver.checkColPlacement(
+      puzzleStrings[0][0],
+      6,
+      9
+    );
+
+    assert.isFalse(validationResult);
+  });
+
+  test('checkRegionPlacement function should return true for a valid region placement', () => {
+    const validationResult = solver.checkRegionPlacement(
+      puzzleStrings[0][0],
+      'I',
+      8,
+      2
+    );
+
+    assert.isTrue(validationResult);
+  });
+
+  test('checkRegionPlacement function should return false for an invalid region placement', () => {
+    const validationResult = solver.checkRegionPlacement(
+      puzzleStrings[0][0],
+      'E',
+      3,
+      1
+    );
+
+    assert.isFalse(validationResult);
   });
 });
