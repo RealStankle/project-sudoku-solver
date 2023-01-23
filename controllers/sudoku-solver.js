4b55314f4d2cf3ee71c2f86e9cd3255cd681abc5
Furkan Emin <femincanx@gmail.com>
2023-01-23 09:56:40 +0300

    feat: add validate controller

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,32 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    if (!puzzleString) {
+      return { result: false, message: 'Required field missing' };
+    }
+
+    if (puzzleString.length !== 81) {
+      return {
+        result: false,
+        message: 'Expected puzzle to be 81 characters long',
+      };
+    }
+
+    const validationRegex = /^(\d|\.)+$/;
+
+    if (!validationRegex.test(puzzleString)) {
+      return { result: false, message: 'Invalid characters in puzzle' };
+    }
+
+    return { result: true };
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
 module.exports = SudokuSolver;
-
