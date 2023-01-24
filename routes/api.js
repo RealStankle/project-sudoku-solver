c6324289af29c83b960c2ea2d3e7c2a27bd4bff3
Furkan Emin <femincanx@gmail.com>
2023-01-24 09:19:26 +0300

    feat: add check route

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,67 @@
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route('/api/check').post((req, res) => {
+    const { puzzle, coordinate, value } = req.body;
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+    if (!puzzle || !coordinate || !value) {
+      return res.json({ error: 'Required field(s) missing' });
+    }
 
-    });
+    const puzzleValidation = solver.validate(puzzle);
+
+    if (!puzzleValidation.result) {
+      return res.json({ error: puzzleValidation.message });
+    }
+
+    if (!/^[A-I]{1}[1-9]{1}$/.test(coordinate)) {
+      return res.json({ error: 'Invalid coordinate' });
+    }
+
+    if (!/^[1-9]{1}$/.test(value)) {
+      return res.json({ error: 'Invalid value' });
+    }
+
+    const rowValidation = solver.checkRowPlacement(
+      puzzle,
+      coordinate[0],
+      value
+    );
+
+    const colValidation = solver.checkColPlacement(
+      puzzle,
+      coordinate[1],
+      value
+    );
+
+    const regionValidation = solver.checkRegionPlacement(
+      puzzle,
+      coordinate[0],
+      coordinate[1],
+      value
+    );
+
+    if (
+      !rowValidation.result ||
+      !colValidation.result ||
+      !regionValidation.result
+    ) {
+      const conflicts = [
+        rowValidation.conflict,
+        colValidation.conflict,
+        regionValidation.conflict,
+      ];
+
+      return res.json({
+        valid: false,
+        conflict: conflicts.filter((item) => item),
+      });
+    }
+
+    res.json({ valid: true });
+  });
+
+  app.route('/api/solve').post((req, res) => {});
 };
