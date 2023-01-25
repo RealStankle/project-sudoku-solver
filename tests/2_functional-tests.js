ed9ca1fbb50fbcf02c2c34e5e09bde7198fd58e2
Furkan Emin <femincanx@gmail.com>
2023-01-25 08:29:34 +0300

    test: adjust functional tests to fit fcc

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -7,169 +7,165 @@ const puzzleStrings = require('../controllers/puzzle-strings');
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
-  suite('POST /api/check', () => {
-    test('it correctly handles request containing all fields', async () => {
-      const response = await chai
-        .request(server)
-        .post('/api/check')
-        .send({ puzzle: puzzleStrings[0][0], coordinate: 'D7', value: 5 });
+  test('POST /api/check, it correctly handles request containing all fields', async () => {
+    const response = await chai
+      .request(server)
+      .post('/api/check')
+      .send({ puzzle: puzzleStrings[0][0], coordinate: 'D7', value: 5 });
 
-      assert.strictEqual(response.status, 200);
-      assert.deepEqual(response.body, { valid: true });
-    });
+    assert.strictEqual(response.status, 200);
+    assert.deepEqual(response.body, { valid: true });
+  });
 
-    test('it correctly handles request with single placement conflict', async () => {
-      const response = await chai
-        .request(server)
-        .post('/api/check')
-        .send({ puzzle: puzzleStrings[0][0], coordinate: 'D4', value: 8 });
+  test('POST /api/check, it correctly handles request with single placement conflict', async () => {
+    const response = await chai
+      .request(server)
+      .post('/api/check')
+      .send({ puzzle: puzzleStrings[0][0], coordinate: 'D4', value: 8 });
 
-      assert.strictEqual(response.status, 200);
-      assert.isFalse(response.body.valid);
-      assert.isArray(response.body.conflict);
-      assert.lengthOf(response.body.conflict, 1);
-      assert.sameMembers(response.body.conflict, ['row']);
-    });
+    assert.strictEqual(response.status, 200);
+    assert.isFalse(response.body.valid);
+    assert.isArray(response.body.conflict);
+    assert.lengthOf(response.body.conflict, 1);
+    assert.sameMembers(response.body.conflict, ['row']);
+  });
 
-    test('it correctly handles request with multiple placement conflicts', async () => {
-      const response = await chai
-        .request(server)
-        .post('/api/check')
-        .send({ puzzle: puzzleStrings[0][0], coordinate: 'G5', value: 4 });
+  test('POST /api/check, it correctly handles request with multiple placement conflicts', async () => {
+    const response = await chai
+      .request(server)
+      .post('/api/check')
+      .send({ puzzle: puzzleStrings[0][0], coordinate: 'G5', value: 4 });
 
-      assert.strictEqual(response.status, 200);
-      assert.isFalse(response.body.valid);
-      assert.isArray(response.body.conflict);
-      assert.lengthOf(response.body.conflict, 2);
-      assert.sameMembers(response.body.conflict, ['row', 'region']);
-    });
+    assert.strictEqual(response.status, 200);
+    assert.isFalse(response.body.valid);
+    assert.isArray(response.body.conflict);
+    assert.lengthOf(response.body.conflict, 2);
+    assert.sameMembers(response.body.conflict, ['row', 'region']);
+  });
 
-    test('it correctly handles request with all placement conflicts', async () => {
-      const response = await chai
-        .request(server)
-        .post('/api/check')
-        .send({ puzzle: puzzleStrings[0][0], coordinate: 'E1', value: 6 });
+  test('POST /api/check, it correctly handles request with all placement conflicts', async () => {
+    const response = await chai
+      .request(server)
+      .post('/api/check')
+      .send({ puzzle: puzzleStrings[0][0], coordinate: 'E1', value: 6 });
 
-      assert.strictEqual(response.status, 200);
-      assert.isFalse(response.body.valid);
-      assert.isArray(response.body.conflict);
-      assert.lengthOf(response.body.conflict, 3);
-      assert.sameMembers(response.body.conflict, ['row', 'column', 'region']);
-    });
+    assert.strictEqual(response.status, 200);
+    assert.isFalse(response.body.valid);
+    assert.isArray(response.body.conflict);
+    assert.lengthOf(response.body.conflict, 3);
+    assert.sameMembers(response.body.conflict, ['row', 'column', 'region']);
+  });
 
-    test('it correctly handles request with missing required fields', async () => {
-      const response = await chai
-        .request(server)
-        .post('/api/check')
-        .send({ coordinate: 'D7', value: 5 });
+  test('POST /api/check, it correctly handles request with missing required fields', async () => {
+    const response = await chai
+      .request(server)
+      .post('/api/check')
+      .send({ coordinate: 'D7', value: 5 });
 
-      assert.strictEqual(response.status, 200);
-      assert.deepEqual(response.body, { error: 'Required field(s) missing' });
-    });
+    assert.strictEqual(response.status, 200);
+    assert.deepEqual(response.body, { error: 'Required field(s) missing' });
+  });
 
-    test('it correctly handles request with invalid characters', async () => {
-      const response = await chai
-        .request(server)
-        .post('/api/check')
-        .send({
-          puzzle: `${puzzleStrings[0][0].slice(0, 78)}abc`,
-          coordinate: 'D7',
-          value: 5,
-        });
-
-      assert.strictEqual(response.status, 200);
-      assert.deepEqual(response.body, {
-        error: 'Invalid characters in puzzle',
+  test('POST /api/check, it correctly handles request with invalid characters', async () => {
+    const response = await chai
+      .request(server)
+      .post('/api/check')
+      .send({
+        puzzle: `${puzzleStrings[0][0].slice(0, 78)}abc`,
+        coordinate: 'D7',
+        value: 5,
       });
-    });
 
-    test('it correctly handles request with incorrect length', async () => {
-      const response = await chai
-        .request(server)
-        .post('/api/check')
-        .send({
-          puzzle: puzzleStrings[0][0].slice(0, 70),
-          coordinate: 'D7',
-          value: 5,
-        });
-
-      assert.strictEqual(response.status, 200);
-      assert.deepEqual(response.body, {
-        error: 'Expected puzzle to be 81 characters long',
-      });
-    });
-
-    test('it correctly handles request with invalid placement coordinate', async () => {
-      const response = await chai
-        .request(server)
-        .post('/api/check')
-        .send({ puzzle: puzzleStrings[0][0], coordinate: 'K24', value: 5 });
-
-      assert.strictEqual(response.status, 200);
-      assert.deepEqual(response.body, { error: 'Invalid coordinate' });
-    });
-
-    test('it correctly handles request with invalid placement value', async () => {
-      const response = await chai
-        .request(server)
-        .post('/api/check')
-        .send({ puzzle: puzzleStrings[0][0], coordinate: 'D7', value: 28 });
-
-      assert.strictEqual(response.status, 200);
-      assert.deepEqual(response.body, { error: 'Invalid value' });
+    assert.strictEqual(response.status, 200);
+    assert.deepEqual(response.body, {
+      error: 'Invalid characters in puzzle',
     });
   });
 
-  suite('POST /api/solve', () => {
-    test('it correctly handles the request with a valid puzzle string', async () => {
-      const response = await chai
-        .request(server)
-        .post('/api/solve')
-        .send({ puzzle: puzzleStrings[2][0] });
-
-      assert.strictEqual(response.status, 200);
-      assert.strictEqual(response.body.solution, puzzleStrings[2][1]);
-    });
-
-    test('it correctly handles the request with the missing puzzle string', async () => {
-      const response = await chai.request(server).post('/api/solve');
-
-      assert.strictEqual(response.status, 200);
-      assert.deepEqual(response.body, { error: 'Required field missing' });
-    });
-
-    test('it correctly handles the request with the puzzle containing invalid characters', async () => {
-      const response = await chai
-        .request(server)
-        .post('/api/solve')
-        .send({ puzzle: `${puzzleStrings[2][0].slice(0, 70)}abcde123456` });
-
-      assert.strictEqual(response.status, 200);
-      assert.deepEqual(response.body, {
-        error: 'Invalid characters in puzzle',
+  test('POST /api/check, it correctly handles request with incorrect length', async () => {
+    const response = await chai
+      .request(server)
+      .post('/api/check')
+      .send({
+        puzzle: puzzleStrings[0][0].slice(0, 70),
+        coordinate: 'D7',
+        value: 5,
       });
+
+    assert.strictEqual(response.status, 200);
+    assert.deepEqual(response.body, {
+      error: 'Expected puzzle to be 81 characters long',
     });
+  });
 
-    test('it correctly handles the request with the incorrect length puzzle', async () => {
-      const response = await chai
-        .request(server)
-        .post('/api/solve')
-        .send({ puzzle: `${puzzleStrings[2][0].slice(0, 60)}` });
+  test('POST /api/check, it correctly handles request with invalid placement coordinate', async () => {
+    const response = await chai
+      .request(server)
+      .post('/api/check')
+      .send({ puzzle: puzzleStrings[0][0], coordinate: 'K24', value: 5 });
 
-      assert.strictEqual(response.status, 200);
-      assert.deepEqual(response.body, {
-        error: 'Expected puzzle to be 81 characters long',
-      });
+    assert.strictEqual(response.status, 200);
+    assert.deepEqual(response.body, { error: 'Invalid coordinate' });
+  });
+
+  test('POST /api/check, it correctly handles request with invalid placement value', async () => {
+    const response = await chai
+      .request(server)
+      .post('/api/check')
+      .send({ puzzle: puzzleStrings[0][0], coordinate: 'D7', value: 28 });
+
+    assert.strictEqual(response.status, 200);
+    assert.deepEqual(response.body, { error: 'Invalid value' });
+  });
+
+  test('POST /api/solve, it correctly handles the request with a valid puzzle string', async () => {
+    const response = await chai
+      .request(server)
+      .post('/api/solve')
+      .send({ puzzle: puzzleStrings[2][0] });
+
+    assert.strictEqual(response.status, 200);
+    assert.strictEqual(response.body.solution, puzzleStrings[2][1]);
+  });
+
+  test('POST /api/solve, it correctly handles the request with the missing puzzle string', async () => {
+    const response = await chai.request(server).post('/api/solve');
+
+    assert.strictEqual(response.status, 200);
+    assert.deepEqual(response.body, { error: 'Required field missing' });
+  });
+
+  test('POST /api/solve, it correctly handles the request with the puzzle containing invalid characters', async () => {
+    const response = await chai
+      .request(server)
+      .post('/api/solve')
+      .send({ puzzle: `${puzzleStrings[2][0].slice(0, 70)}abcde123456` });
+
+    assert.strictEqual(response.status, 200);
+    assert.deepEqual(response.body, {
+      error: 'Invalid characters in puzzle',
     });
+  });
 
-    test('it correctly handles the request with the puzzle that cannot be solved', async () => {
-      const response = await chai
-        .request(server)
-        .post('/api/solve')
-        .send({ puzzle: `${puzzleStrings[2][0].slice(0, 75)}167843` });
+  test('POST /api/solve, it correctly handles the request with the incorrect length puzzle', async () => {
+    const response = await chai
+      .request(server)
+      .post('/api/solve')
+      .send({ puzzle: `${puzzleStrings[2][0].slice(0, 60)}` });
 
-      assert.strictEqual(response.status, 200);
-      assert.deepEqual(response.body, { error: 'Puzzle cannot be solved' });
+    assert.strictEqual(response.status, 200);
+    assert.deepEqual(response.body, {
+      error: 'Expected puzzle to be 81 characters long',
     });
+  });
+
+  test('POST /api/solve, it correctly handles the request with the puzzle that cannot be solved', async () => {
+    const response = await chai
+      .request(server)
+      .post('/api/solve')
+      .send({ puzzle: `${puzzleStrings[2][0].slice(0, 75)}167843` });
+
+    assert.strictEqual(response.status, 200);
+    assert.deepEqual(response.body, { error: 'Puzzle cannot be solved' });
   });
 });
